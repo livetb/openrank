49de5dcaac3da77541c984877856592b1e816ddf
livetb <livetb>
2022-01-11 12:42:13 +0800

    ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9094aacdae789dccd67fa32109ff1a18",
-"index.html": "e704fa3b871b9230a45f99e90371da09",
-"/": "e704fa3b871b9230a45f99e90371da09",
-"main.dart.js": "ff02c2cf5d3075b42dca7712e141f131",
+"index.html": "039e77e8b3eaf372e2ef135a947f06cf",
+"/": "039e77e8b3eaf372e2ef135a947f06cf",
+"main.dart.js": "32b43b04aba5a8562ef4a97bafbb6ab1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
